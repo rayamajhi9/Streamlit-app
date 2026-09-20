c932fc33da12cf48c42f637aa7332eaf98ef7b7d
rayamajhi9 <rayamajhi.ajay@gmail.com>
2026-09-20 09:06:22 +0545

    mod

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ conn = st.connection("gsheets", type=GSheetsConnection)
 
 data = conn.read(
     spreadsheet="https://docs.google.com/spreadsheets/d/1KwRByJIUpQ4Ff8c1E-dj9EIPRpjeOno9QzqSfz0JGTk/edit?gid=221719433#gid=221719433",
-    worksheet="Expenses tracker",
+    worksheet="Expenses tracker"
 )
 st.dataframe(data)
