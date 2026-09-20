6f856440be078d89aff37a0ed44818d16e562e67
rayamajhi9 <rayamajhi.ajay@gmail.com>
2026-09-20 09:07:04 +0545

    changes to be committed:data = conn.read(     worksheet="Expenses tracker" )

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from streamlit_gsheets import GSheetsConnection
 conn = st.connection("gsheets", type=GSheetsConnection)
 
 data = conn.read(
-    spreadsheet="https://docs.google.com/spreadsheets/d/1KwRByJIUpQ4Ff8c1E-dj9EIPRpjeOno9QzqSfz0JGTk/edit?gid=221719433#gid=221719433",
     worksheet="Expenses tracker"
 )
 st.dataframe(data)
