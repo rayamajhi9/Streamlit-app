09b5cded69c382d2e240b9ff046266fd98a8094d
rayamajhi9 <rayamajhi.ajay@gmail.com>
2026-09-20 08:53:06 +0545

    fixing the connection to google sheets

--- app.py
+++ app.py
@@ -3,5 +3,8 @@ from streamlit_gsheets import GSheetsConnection
 
 conn = st.connection("gsheets", type=GSheetsConnection)
 
-data = conn.read(worksheet="Expenses tracker")
+data = conn.read(
+    spreadsheet="https://docs.google.com/spreadsheets/d/1KwRByJIUpQ4Ff8c1E-dj9EIPRpjeOno9QzqSfz0JGTk/edit?gid=221719433#gid=221719433",
+    worksheet="Expenses tracker",
+)
 st.dataframe(data)
