67e2ea11ee1af0d275c6db0c06543fa6806999c8
rayamajhi9 <rayamajhi.ajay@gmail.com>
2026-09-21 23:11:34 +0545

    pages added for expenses dashboard and chatbot. Also added a new view for expenses tracker which reads data from Google Sheets using the GSheetsConnection. The app.py file has been updated to include the new pages and the authorization requirement.

--- app.py
+++ app.py
@@ -1,12 +1,46 @@
 import streamlit as st
-from streamlit_gsheets import GSheetsConnection
+
 from auth import require_authorization
 
 require_authorization()
 
-conn = st.connection("gsheets", type=GSheetsConnection)
-
-data = conn.read(
-    worksheet="Expenses tracker"
+# -- PAGE SETUP ---
+expense_view = st.Page(
+    page="views/expense_view.py",
+    title="Expense Tracker", 
+    icon=":material/account_circle:",
+    layout="wide",
 )
-st.dataframe(data)
+
+project_1_page = st.Page(
+    page="views/expenses_dashboard.py",
+    title="Expenses Dashboard",
+    icon=":material/bar_chart:",
+)
+
+project_2_page = st.Page(
+    page="views/chatbot.py",
+    title="Chatbot",
+    icon=":material/smart_toy:",
+)
+
+pg = st.navigation(
+    pages=[expense_view, project_1_page, project_2_page],
+    default=expense_view,
+    title="Expense Tracker",
+    icon=":material/account_circle:",
+    layout="wide",
+)
+
+pg.run()
+
+# data = conn.read(
+#    worksheet="Expenses tracker"
+#)
+#st.dataframe(data)
+
+
+#data = conn.read(
+#    worksheet="Expense Register"
+#)
+#st.dataframe(data)
