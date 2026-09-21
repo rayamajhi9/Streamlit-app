8425b58acefad525b98ebe0a8b31bf21fc9dd9cf
rayamajhi9 <rayamajhi.ajay@gmail.com>
2026-09-21 23:14:54 +0545

    pages py file

---
+++ views/expenses_views.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+
+data = conn.read(
+    worksheet="Expenses tracker"
+)
+st.dataframe(data)
